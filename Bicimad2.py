ab9b381f60ecca1765ab55c11fd9dde0fb96c6f6
alvaroseco <98521098+alvaroseco@users.noreply.github.com>
2022-05-19 12:36:03 +0200

    Probrar

--- Bicimad2.py
+++ Bicimad2.py
@@ -13,36 +13,29 @@ sc = SparkContext()
 
 def mapper_edad(line):
     data = json.loads(line)
-    _id = data['_id']
-    estacion_salida = data['idunplug_station']
-    estacion_llegada = data['idplug_station']
-    franja_horaria = data['unplug_hourTime']
     tiempo_viaje = data['travel_time']
-    tipo_usuario = data['user_type']
     rango_edad = data['ageRange']
     return rango_edad,tiempo_viaje
 
 def mapper_usuario(line):
     data = json.loads(line)
-    _id = data['_id']
-    estacion_salida = data['idunplug_station']
-    estacion_llegada = data['idplug_station']
-    franja_horaria = data['unplug_hourTime']
     tiempo_viaje = data['travel_time']
     tipo_usuario = data['user_type']
-    rango_edad = data['ageRange']
     return tipo_usuario,tiempo_viaje
+
+def mapper_plugstation(line):
+    data = json.loads(line)
+    plug = data['idplug_base']
+    return plug,1
+
+def mapper_unplugstation(line):
+    data = json.loads(line)
+    unplug = data['idunplug_base']
+    return unplug,1
 
 def mapper_usuario_unico(line):
     data = json.loads(line)
-    _id = data['_id']
     codigo_usuario = data['user_day_code']
-    estacion_salida = data['idunplug_station']
-    estacion_llegada = data['idplug_station']
-    franja_horaria = data['unplug_hourTime']
-    tiempo_viaje = data['travel_time']
-    tipo_usuario = data['user_type']
-    rango_edad = data['ageRange']
     return codigo_usuario,1
 
 def crear_lista_rdd(lista):
@@ -60,6 +53,8 @@ def crear_lista(lista):
         result1.append(i[0])
         result2.append(i[1])
     return [result1,result2]
+
+#-------------------------------------------------------------------------------------------------
 
 def estudio_edad(rdd_base,archivo_salida):
 	#Filtramos por edad, y nos quedamos con tupla (edad,list tiempo), despues calculamos media
@@ -83,6 +78,49 @@ def estudio_usuario(rdd_base,archivo_salida):
 	plt.title('Tiempo medio dependiendo del usuario')
 	matplotlib.pyplot.show()
     
+#----------------------------------------------------------------------------------------------------
+
+def asociar_barrio(tupla):
+    if tupla[0] <= 63 and tupla[0] >= 1:
+        nodo = 'Centro'
+    elif (tupla[0] >= 116 and tupla[0] <= 126) or tupla[0] == 132:
+        nodo = 'Moncloa'
+    elif tupla[0] >= 139 and tupla[0] <= 143:
+        nodo = 'Tetuán'
+    elif tupla[0] >= 156 and tupla[0] <= 161:
+        nodo = 'Chamberí'
+    elif (tupla[0] >= 144 and tupla[0] <= 155) or (tupla[0] >= 168 and tupla[0] <= 173) or (tupla[0]== 137) or (tupla[0]==138):
+        nodo = 'Chamartin'
+    elif (tupla[0] >= 92 and tupla[0] <= 115) or (tupla[0] >= 162 and tupla[0] <= 167):
+        nodo = 'Salamanca'
+    elif (tupla[0] >= 64 and tupla[0] <= 91):
+        nodo = 'Retiro'
+    else:
+        nodo = 'Arganzuela'
+    return nodo,tupla[1]
+    
+def estudio_station(rdd_base,archivo_salida):
+    
+    rdd_plugstation = rdd_base.map(mapper_plugstation).map(asociar_barrio).groupByKey().map(lambda x : (x[0],sum(list(x[1])))).collect()
+    #rdd_unplugstation = rdd_base.map(mapper_unplugstation).map(asociar_barrio).groupByKey().map(lambda x : (x[0],sum(list(x[1])))).collect()
+    archivo_salida.write(str(rdd_plugstation))
+    #archivo_salida.write(str(rdd_unplugstation))
+    ejes_plug = crear_lista(list(rdd_plugstation))
+    #ejes_unplug = crear_lista(list(rdd_unplugstation))
+    fig, ax =plt.subplots(1,1)
+    data = []
+    for i in range(len(ejes_plug[1])): 
+        data.append([ejes_plug[1][i]])#,ejes_unplug[1][i]])
+    column_labels=["Plug Station", "Unplug Station"]
+    ax.axis('tight')
+    ax.axis('off')
+    ax.table(cellText=data,rowLabels=ejes_plug[0],colLabels=column_labels,loc="center")
+
+    plt.show()
+
+
+#----------------------------------------------------------------------------------------------------------
+
 
 def estudio_usuario_unico(lrdd,archivo_salida):
     usuarios = []
@@ -102,7 +140,8 @@ def estudio_usuario_unico(lrdd,archivo_salida):
 def proceso(rdd,archivo_salida,lrdd):
     #estudio_usuario(rdd,archivo_salida)
     #estudio_edad(rdd,archivo_salida)
-    estudio_usuario_unico(lrdd,archivo_salida)
+    #estudio_usuario_unico(lrdd,archivo_salida)
+    estudio_station(rdd, archivo_salida)
 
 def main(sc, years, months):
     rdd = sc.parallelize([])
