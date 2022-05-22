9bd456b6036c47c548b940f9ad37bde385e3e564
luciabp <98393692+luciabp@users.noreply.github.com>
2022-05-22 13:13:17 +0200

    Add files via upload

--- Bicimad9.py
+++ Bicimad9.py
@@ -1,13 +1,11 @@
+
 from pyspark import SparkContext
 import json
 import sys
-from pprint import pprint
-import statistics
 import matplotlib
 import matplotlib.pyplot as plt
 
-
-from matplotlib.pyplot import boxplot
+#-------------------------------------------------------------------------------------------------
 
 sc = SparkContext()
 
@@ -38,6 +36,8 @@ def mapper_usuario_unico(line):
     codigo_usuario = data['user_day_code']
     return codigo_usuario,1
 
+#-------------------------------------------------------------------------------------------------
+    
 def crear_lista_rdd(lista):
     result1 = []
     result2 = []
@@ -55,60 +55,10 @@ def crear_lista(lista):
     return [result1,result2]
 
 #-------------------------------------------------------------------------------------------------
-def estudio_edad(rdd19, rdd20, archivo_salida):
-    #2019
-	rdd19_edad_media = rdd19.map(mapper_edad).groupByKey().map(lambda x : (x[0],sum(list(x[1]))/len(list(x[1])))).collect()
-	archivo_salida.write(str(rdd19_edad_media))
-	ejes19 = crear_lista(list(rdd19_edad_media))
-    #2020
-	rdd20_edad_media = rdd20.map(mapper_edad).groupByKey().map(lambda x : (x[0],sum(list(x[1]))/len(list(x[1])))).collect()
-	archivo_salida.write(str(rdd20_edad_media))
-	ejes20 = crear_lista(list(rdd20_edad_media))
+
+#Los distintos distritos con sus barrios 
+#En cada barrio tenemos una lista de los distintos codigos de Bicimad que hay
     
-    # GRAFICO DE BARRAS
-	bar_width = 0.3 #ancho de las barras
-	ejes20_2 = []
-    #Para poner las barras una detrás de la otra
-	for i in ejes20[0]:
-		ejes20_2.append(i+bar_width)
-        
-    #Lo pasamos a minutos
-	for i in range(0,len(ejes19[1])):
-		ejes19[1][i] = ejes19[1][i]/60
-		ejes20[1][i] = ejes20[1][i]/60
-        
-	matplotlib.pyplot.bar(ejes19[0],ejes19[1],bar_width,color='b',label='2019')
-	matplotlib.pyplot.bar(ejes20_2,ejes20[1],bar_width,color='r',label='2020')
-	plt.legend(loc='best')
-	plt.title('Tiempo medio dependiendo de la edad')
-	plt.show()
-
-
-def estudio_usuario_unico(lrdd19, lrdd20, months,archivo_salida):
-    usuarios19 = []
-    usuarios20 = []
-    for i in range(len(months)):
-        rdd19_usuario_unico = (lrdd19[i]).map(mapper_usuario_unico).groupByKey().collect()
-        archivo_salida.write(str(rdd19_usuario_unico))
-        usuario19 = len((list(rdd19_usuario_unico)))
-        usuarios19.append(usuario19)
-        rdd20_usuario_unico = (lrdd20[i]).map(mapper_usuario_unico).groupByKey().collect()
-        archivo_salida.write(str(rdd20_usuario_unico))
-        usuario20 = len((list(rdd20_usuario_unico)))
-        usuarios20.append(usuario20)
-    # GRAFICO
-    bar_width = 0.3
-    mes2 = []
-    for i in months:
-        mes2.append(i+bar_width)
-    matplotlib.pyplot.bar(months,usuarios19,bar_width,color='b',label='2019')
-    matplotlib.pyplot.bar(mes2,usuarios20,bar_width,color='r',label='2020')
-    plt.legend(loc='best')
-    plt.title('Número de usuarios únicos')
-    plt.show()
-    
-#----------------------------------------------------------------------------------------------------
-
 #Distrito 1 - Centro
 Palacio = [9,23,24,30,35,36,37,38,39,166]
 Embajadores = [40,41,42,43,44,45,48,49,50,51,53]
@@ -116,6 +66,7 @@ Cortes = [27,28,29,34,52,67,86]
 Justicia = [5,6,7,10,18,19,20,26,54,58]
 Universidad =[2,4,11,12,13,15,16,17,55,57,59,211]
 Sol = [1,22,25,31,32,33,56,210,64]
+
 #Distrito 2 - Arganzuela
 Imperial = [167,236]
 Acacias = [46,47,162,163,169,170,173,174]
@@ -124,6 +75,7 @@ Legazpi = [235]
 Delicias = [164,171,176,177]
 Palos_de_Moguer = [118,120,121,168,172]
 Atocha = [80,119,180]
+
 #Distrito 3 - Retiro
 Pacifico = [77,79,178]
 Adelfas = [78,84]
@@ -131,6 +83,7 @@ Estrella = [73,84,182,82]
 Ibiza = [61,62,63,181]
 Jeronimos = [60,65,68,69,70,74,75,81,85]
 Niño_Jesus = [71,72,76,83]
+
 #Distrito 4 - Salamanca
 Recoletos = [87,90,92,93,94,95,96,102,104,105,106,107]
 Goya = [89,91,97,98,99,100,101,108,183]
@@ -138,6 +91,7 @@ Fuente_del_Ebro = [184,185,187,66]
 Guindalera = [191,192,193,194,240,241,242,243,21]
 Lista = [143,144,145,186]
 Castellana = [88,103,109,142,190]
+
 #Distrito 5 - Chamartin
 El_Viso = [136,137,139,140,146,147,148,149,150,197,250]
 Prosperidad = [195,251]
@@ -145,12 +99,14 @@ Ciudad_Jardin = [196]
 Hispanoamerica = [158,159,160,253]
 Nueva_españa = [157,209,254,255]
 Castilla = [208,249,252]
+
 #Distrito 6 - Tetuan
 Bellas_vistas = [205,237]
 Cuatro_Caminos = [133,151,152,153,154,155,238]
 Castillejos = [156]
 Valdeacederas = [207]
 Berruguete = [206,239]
+
 #Distrito 7 - Chamberi
 Gaztambide = [239,111,123,128,199]
 Arapiles = [3,127,200,245]
@@ -158,34 +114,42 @@ Trafalgar = [124,126,246]
 Almagro = [8,122,125,141,198]
 Rios_Rosas = [129,130,131,132,134,135,138,201,247]
 Vallehermoso = [202,203,244]
+
 #Distrito 8 - Fuencarral 
 La_Paz = [217,248]
+
 #Distrito 9 - Moncloa
 Casa_de_Campo = [110,113,161]
 Arguelles = [14,112,114,115,116,117]
 Ciudad_Universitaria = [256,257,258,259,260,261]
+
 #Distrito 10 - Latina
 Carmenes = [223]
 Puerta_del_Angel = [216,224]
+
 #Distrito 11 - Carabanchel
 Comilla = [228]
 San_Isidro = [212]
+
 #Distrito 12 - Usera
 Moscardo = [213,222]
+
 #Distrito 13 - Puente de Vallecas
 San_Diego = [179,227]
 Numancia = [225,226]
+
 #Distrito 14 - Moratalaz
 Pavones = [221]
 Marroquina = [219]
 Media_Legua = [218,220]
+
 #Distrito 15 - Ciudad Lineal
 Ventas = [188,232,233,234]
 Pueblo_Nuevo = [215]
 Concepcion = [189,229]
 San_Pascual = [214,230,231]
 
-Barrios = {'nombres' : ['Palacio','Embajardores','Cortes','Justicia','Universidad','Sol',
+Barrios = {'nombres' : ['Palacio','Embajadores','Cortes','Justicia','Universidad','Sol',
                         'Imperial','Acacias','Chopera','Legazpi','Delicias','Palos_de_Moguer','Atocha',
                         'Pacifico','Adelfas','Estrella','Ibiza','Jeronimos','Niño_Jesus',
                         'Recoletos','Goya','Fuente_del_Ebro','Guindalera','Lista','Castellana',
@@ -216,24 +180,92 @@ Barrios = {'nombres' : ['Palacio','Embajardores','Cortes','Justicia','Universida
                          Pavones,Marroquina,Media_Legua,
                          Ventas,Pueblo_Nuevo,Concepcion,San_Pascual]}
 
-
 def asociar_barrio(tupla):
     for i in range(len(Barrios['lista'])):
         if tupla[0] in Barrios['lista'][i]:
             nodo = Barrios['nombres'][i]
             return nodo,tupla[1]
     return"nada",1
+    
+#----------------------------------------------------------------------------------------------------
+    #Funcion para obtener un estudio del tiempo que invierten los usuarios en la bici en funcion de su edad
+def estudio_edad(rdd19, rdd20, archivo_salida):
+    
+    #Datos 2019
+	rdd19_edad_media = rdd19.map(mapper_edad).groupByKey().map(lambda x : (x[0],sum(list(x[1]))/len(list(x[1])))).collect()
+	archivo_salida.write(str(rdd19_edad_media))
+	ejes19 = crear_lista(list(rdd19_edad_media))
+    
+    #Datos 2020
+	rdd20_edad_media = rdd20.map(mapper_edad).groupByKey().map(lambda x : (x[0],sum(list(x[1]))/len(list(x[1])))).collect()
+	archivo_salida.write(str(rdd20_edad_media))
+	ejes20 = crear_lista(list(rdd20_edad_media))
+    
+    # Representamos el grafico de barras
+	bar_width = 0.3 #ancho de las barras
+    
+    #Para poner las barras una detrás de la otra
+	for i in ejes20[0]:
+		ejes20[0][i] = ejes20[0][i] + bar_width
+
+    #Buscamos tener el eje Y en minutos
+	for i in range(0,len(ejes19[1])):
+		ejes19[1][i] = ejes19[1][i]/60
+		ejes20[1][i] = ejes20[1][i]/60
+        
+	matplotlib.pyplot.bar(ejes19[0],ejes19[1],bar_width,color='b',label='2019')
+	matplotlib.pyplot.bar(ejes20[0],ejes20[1],bar_width,color='r',label='2020')
+	plt.legend(loc='best')
+	plt.title('Tiempo medio dependiendo de la edad')
+	plt.show()
+
+#-------------------------------------------------------------------------------------------------
+     #Funcion para obtener un estudio del numero de usuarios únicos
+def estudio_usuario_unico(lrdd19, lrdd20, months,archivo_salida):
+    
+    usuarios19 = []
+    usuarios20 = []
+    
+    for i in range(len(months)):
+        #2019
+        rdd19_usuario_unico = (lrdd19[i]).map(mapper_usuario_unico).groupByKey().collect()
+        archivo_salida.write(str(rdd19_usuario_unico))
+        usuario19 = len((list(rdd19_usuario_unico)))
+        usuarios19.append(usuario19)
+        #2020
+        rdd20_usuario_unico = (lrdd20[i]).map(mapper_usuario_unico).groupByKey().collect()
+        archivo_salida.write(str(rdd20_usuario_unico))
+        usuario20 = len((list(rdd20_usuario_unico)))
+        usuarios20.append(usuario20)
+        
+    # Representamos el grafico de barras
+    bar_width = 0.3 #ancho de las barras
+    
+    #Para poner las barras una detrás de la otra
+    mes2 = []
+    for i in months:
+        mes2.append(i+bar_width)
+        
+    matplotlib.pyplot.bar(months,usuarios19,bar_width,color='b',label='2019')
+    matplotlib.pyplot.bar(mes2,usuarios20,bar_width,color='r',label='2020')
+    plt.legend(loc='best')
+    plt.title('Número de usuarios únicos')
+    plt.show()
+    
+#--------------------------------------------------------------------------------------------
 
 def estudio_station(rdd19,rdd20,archivo_salida):
     
-    rdd_unplugstation19 = rdd19.map(mapper_unplugstation).map(asociar_barrio).sortByKey(True,1).groupByKey().map(lambda x : (x[0],sum(list(x[1])))).filter(lambda x : x[0]!="nada").collect()
-    rdd_unplugstation20 = rdd20.map(mapper_unplugstation).map(asociar_barrio).sortByKey(True,1).groupByKey().map(lambda x : (x[0],sum(list(x[1])))).filter(lambda x : x[0]!="nada").collect()
-    rdd_plugstation19 = rdd19.map(mapper_plugstation).map(asociar_barrio).sortByKey(True,1).groupByKey().map(lambda x : (x[0],sum(list(x[1])))).filter(lambda x : x[0]!="nada").collect()
-    rdd_plugstation20 = rdd20.map(mapper_plugstation).map(asociar_barrio).sortByKey(True,1).groupByKey().map(lambda x : (x[0],sum(list(x[1])))).filter(lambda x : x[0]!="nada").collect()
+    rdd_unplugstation19 = rdd19.map(mapper_unplugstation).map(asociar_barrio).sortByKey(True,1).groupByKey().map(lambda x : (x[0],sum(list(x[1])))).filter(lambda x : x[0]!="nada").sortBy(lambda x:-x[1]).collect()
+    rdd_unplugstation20 = rdd20.map(mapper_unplugstation).map(asociar_barrio).sortByKey(True,1).groupByKey().map(lambda x : (x[0],sum(list(x[1])))).filter(lambda x : x[0]!="nada").sortBy(lambda x:-x[1]).collect()
+    rdd_plugstation19 = rdd19.map(mapper_plugstation).map(asociar_barrio).sortByKey(True,1).groupByKey().map(lambda x : (x[0],sum(list(x[1])))).filter(lambda x : x[0]!="nada").sortBy(lambda x:-x[1]).collect()
+    rdd_plugstation20 = rdd20.map(mapper_plugstation).map(asociar_barrio).sortByKey(True,1).groupByKey().map(lambda x : (x[0],sum(list(x[1])))).filter(lambda x : x[0]!="nada").sortBy(lambda x:-x[1]).collect()
+    
     archivo_salida.write(str(rdd_unplugstation19))
     archivo_salida.write(str(rdd_unplugstation20))
     archivo_salida.write(str(rdd_plugstation19))
     archivo_salida.write(str(rdd_plugstation20))
+    
     ejes_unplug19 = crear_lista(list(rdd_unplugstation19))
     ejes_unplug20 = crear_lista(list(rdd_unplugstation20))
     ejes_plug19 = crear_lista(list(rdd_plugstation19))
@@ -273,6 +305,7 @@ def estudio_station(rdd19,rdd20,archivo_salida):
     plt.show()
 
 #----------------------------------------------------------------------------------------------------------
+
 def proceso(rdd19,rdd20,lrdd19,lrdd20, months, archivo_salida):
     estudio_edad(rdd19, rdd20, archivo_salida)
     estudio_usuario_unico(lrdd19, lrdd20, months, archivo_salida)
@@ -304,11 +337,11 @@ def main(sc, years, months):
 
 if __name__ =="__main__":
 	if len(sys.argv) <= 1:
-		years=[2019,2020]
+		years=[2019,2020] 
 	else:
 		years=list(map(int, sys.argv[1][1:-1].split(",")))
 	if len(sys.argv) <= 2:
-		months=[5,6,7,8,9,10]
+		months=[5,6,7,8,9,10] #Los meses de los que hacemos el estudio
 		
 	else:
 		months=list(map(int, sys.argv[2][1:-1].split(",")))
